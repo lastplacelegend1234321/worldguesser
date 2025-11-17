498634fb4093cdec61097909a5c0f5345bcf853c
Michaelpritsky001 <michaelpritsky@gmail.com>
2025-11-16 17:02:49 -0800

    Add WebSocket URL logging for debugging connection issues

--- components/home.js
+++ components/home.js
@@ -1268,7 +1268,9 @@ export default function Home({ }) {
                                 currentRetry: currentAttempt
                             }))
 
-                            ws = await initWebsocket(clientConfig().websocketUrl, null, 5000, 0) // 0 retries, we handle it ourselves
+                            const wsUrl = clientConfig().websocketUrl;
+                            console.log("Attempting WebSocket connection to:", wsUrl);
+                            ws = await initWebsocket(wsUrl, null, 5000, 0) // 0 retries, we handle it ourselves
                             break;
                         } catch (error) {
                             console.log(`Connection attempt ${currentAttempt}/${maxAttempts} failed`);
